e270e8ae8cdf239174e7c3ea6b651de7f7c2ba80
John Issac <jijohnissac@gmail.com>
2026-03-18 22:30:06 +0530

    Added clean import setup

--- src/utils/wallet.js
+++ src/utils/wallet.js
@@ -3,25 +3,34 @@ globalThis.Buffer = Buffer;
 
 import * as bip39 from "bip39";
 import { HDKey } from "micro-ed25519-hdkey";
-import nacl from "tweetnacl";
 import { Keypair } from "@solana/web3.js";
 
-export const createWallet = async (index = 0) => {
-    const mnemonic = bip39.generateMnemonic();
-    const seed = await bip39.mnemonicToSeed(mnemonic);
+async function generate(mnemonic, index = 0) {
+  const seed = await bip39.mnemonicToSeed(mnemonic);
 
-    const hd = HDKey.fromMasterSeed(seed);
+  const hd = HDKey.fromMasterSeed(seed);
 
-    const path = `m/44'/501'/${index}'/0'`;
-    const derived = hd.derive(path);
+  const path = `m/44'/501'/${index}'/0'`;
+  const derived = hd.derive(path);
 
-    // const seed32 = seed.slice(0, 32);
-    const keypair = Keypair.fromSeed(derived.privateKey);
+  // const seed32 = seed.slice(0, 32);
+  const keypair = Keypair.fromSeed(derived.privateKey);
 
-    return {
-        seed,
-        mnemonic,
-        publicKey: keypair.publicKey.toString(),
-        secretKey: Array.from(keypair.secretKey)
-    }
+  return {
+    seed,
+    mnemonic,
+    publicKey: keypair.publicKey.toString(),
+    secretKey: Array.from(keypair.secretKey),
+  };
 }
+
+const createWallet = async () => {
+  const mnemonic = bip39.generateMnemonic();
+  return await generate(mnemonic);
+};
+
+const importWalletMnemonic = async (mnemonic) => {
+  return await generate(mnemonic);
+};
+
+export { createWallet, importWalletMnemonic };
